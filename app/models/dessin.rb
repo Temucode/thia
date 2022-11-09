a19e75922d8d16c838c634966cd6da7335a7b5c6
magvan temuulen <magvan.temuulen@gmail.com>
2022-11-09 17:14:30 +0100

    show fixed

--- app/models/dessin.rb
+++ app/models/dessin.rb
@@ -1,2 +1,7 @@
 class Dessin < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :description, presence: true
+  validates :description, uniqueness: true
+  has_many :reviews, dependent: :destroy
 end
