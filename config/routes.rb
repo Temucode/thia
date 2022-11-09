0bc5139e07150d3de001793c0f673dda6c92f48b
magvan temuulen <magvan.temuulen@gmail.com>
2022-11-09 12:22:30 +0100

    creation model et routers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get "dessins", to: "dessins#index"
+  # get "dessins/new", to: "dessins#new"
+  # get "dessins/:id/edit", to: "dessins#edit"
+  # post "dessins", to: "dessins#create"
+  # get "dessins/:id", to: "dessins#show", as: :dessin
+  # patch "dessins/:id", to: "dessins#update"
+  # delete "dessins/:id", to: "dessins#destroy"
+  resources :dessins do
+    resources :reviews, only: [:index, :new, :create]
+  end
+  resources :reviews, only: [:show, :edit, :update, :destroy]
 end
