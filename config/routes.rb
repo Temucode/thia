797ebca9e45a17930751a1f279d1f04862e4a9a1
magvan temuulen <magvan.temuulen@gmail.com>
2022-11-23 21:07:54 +0100

    box-shadow

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'reviews/new'
+  # get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
