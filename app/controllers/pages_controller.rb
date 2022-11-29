255c6c6db5b6bc0efb019ca706221cea8732c31c
magvan temuulen <magvan.temuulen@gmail.com>
2022-11-29 15:41:01 +0100

    modal home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @dessins = Dessin.all.sample(3)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,5 @@ Rails.application.routes.draw do
     resources :reviews, only: [:index, :new, :create]
   end
   resources :reviews, only: [:show, :edit, :update, :destroy]
+
 end
