ad4efee2d009eead25e0c0be8424312dd24fd924
Kevin Trinh <kxt.trinh@gmail.com>
2022-12-27 17:42:13 +0100

    cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,24 @@ puts "Cleaning database..."
 Dessin.destroy_all
 User.destroy_all
 
+images_seed = []
+
 p "creating user"
 user = User.create!(email: "test1@mail.com", password: "test123")
 
 p "creating dessin"
+
+images_seed =[ "./app/assets/images/img-thia.jpeg",
+              "./app/assets/images/img-thia1.jpeg",
+              "./app/assets/images/img-thia2.jpeg",
+              "./app/assets/images/img-thia3.jpeg",
+              "./app/assets/images/img-thia4.jpeg",
+              "./app/assets/images/img-thia5.jpeg",
+              "./app/assets/images/img-thia6.jpeg",
+              ]
+
 10.times do
-  file = URI.open("./app/assets/images/img-thia.jpeg")
+  file = URI.open(images_seed.sample)
   dessin = Dessin.create!(
                           name: Faker::Name.name,
                           description: Faker::Lorem.sentences(number: 1),
